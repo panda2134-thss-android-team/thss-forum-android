9535578f6c72add7f54ab4da1a25b8d6f33e4b90
EdwardFrog <changkehua@gmail.com>
2022-05-26 18:01:59 +0800

    fuel test

--- app/src/main/java/com/example/campusforum/MainActivity.kt
+++ app/src/main/java/com/example/campusforum/MainActivity.kt
@@ -9,32 +9,20 @@ import com.google.gson.reflect.TypeToken;
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
 import com.example.campusforum.models.User
+import com.github.kittinunf.fuel.gson.responseObject
+
 
 class MainActivity : AppCompatActivity() {
-    private fun <T> jsonToList(jsonList: String): List<T> {
-        val gson = Gson()
-        return gson.fromJson(jsonList, object : TypeToken<ArrayList<T>>() {}.type)
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val httpAsync = "http://183.172.58.70:4523/mock/934436/profile/following/"
-            .httpGet()
-            .responseString { request, response, result ->
-                when (result) {
-                    is Result.Failure -> {
-                        val ex = result.getException()
-                        println(ex)
-                    }
-                    is Result.Success -> {
-                        val data = result.get()
-                        val usersList = jsonToList<User>(data)
-                        println(data)
-                        println(usersList.first().nickname)
-                    }
+        Fuel.get("http://183.172.58.70:4523/mock/934436/profile/following/").responseObject<List<User>> { _, _, result ->
+            val (userList, error) = result
+            if (userList != null) {
+                for (user in userList) {
+                    println("nickname : ${user.nickname}, uid: ${user.uid}")
                 }
             }
-
-        httpAsync.join()
+        }
     }
 }
