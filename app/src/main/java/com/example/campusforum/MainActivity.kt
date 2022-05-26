3d2bc9e84da8a41112c9f7daf4a0728a1686fbeb
EdwardFrog <changkehua@gmail.com>
2022-05-26 17:12:49 +0800

    gson test & mdoels update

--- app/src/main/java/com/example/campusforum/MainActivity.kt
+++ app/src/main/java/com/example/campusforum/MainActivity.kt
@@ -3,15 +3,21 @@ package com.example.campusforum
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.github.kittinunf.fuel.Fuel
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
 import com.example.campusforum.models.User
 
 class MainActivity : AppCompatActivity() {
+    private fun <T> jsonToList(jsonList: String): List<T> {
+        val gson = Gson()
+        return gson.fromJson(jsonList, object : TypeToken<ArrayList<T>>() {}.type)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val httpAsync = "http://183.172.58.70:4523/mock/934436/profile/following/"
             .httpGet()
             .responseString { request, response, result ->
@@ -22,7 +28,9 @@ class MainActivity : AppCompatActivity() {
                     }
                     is Result.Success -> {
                         val data = result.get()
+                        val usersList = jsonToList<User>(data)
                         println(data)
+                        println(usersList.first().nickname)
                     }
                 }
             }
