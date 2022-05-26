dd265c61f902505c5c9928884860a4c2f19222c8
EdwardFrog <changkehua@gmail.com>
2022-05-26 14:49:46 +0800

    update models

--- app/src/main/java/com/example/campusforum/MainActivity.kt
+++ app/src/main/java/com/example/campusforum/MainActivity.kt
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.httpGet
-import com.github.kittinunf.result.Result;
+import com.github.kittinunf.result.Result
+import com.example.campusforum.models.User
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val httpAsync = "http://183.172.61.34:4523/mock/934436/profile/following/"
+
+        val httpAsync = "http://183.172.58.70:4523/mock/934436/profile/following/"
             .httpGet()
             .responseString { request, response, result ->
                 when (result) {
